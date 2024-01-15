7e0be52ca5d85119616f2d0dcc7146fad606e301
SamaraSilva23 <samarasilva1173@gmail.com>
2024-01-15 20:34:18 -0300

    primeiro commit

---
+++ Funcao.py
@@ -0,0 +1,22 @@
+def adicao(x, y):
+    adicao = (x + y)
+    return adicao 
+def subtracao(x,y):
+    subtracao = (x -y)
+    return subtracao
+def multiplicacao(x,y):
+    multiplicacao = (x * y)
+    return multiplicacao
+def divicao(x,y):
+    divicao = (x / y)
+    return divicao
+
+a = adicao(3, 25)
+s = subtracao(3, 25)
+m = multiplicacao(3, 25)
+d = divicao(3, 25)
+
+print(a)
+print(s)
+print(m)
+print(d)
